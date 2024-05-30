9b5064e3c20a4659c54b6ded415d36187fa4fd93
Mani Deepak <mattaparthisaimanideepak@gmail.com>
2024-05-30 17:29:39 +0530

    First Commit

---
+++ src/Time.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+public class Time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Time is:"+LocalTime.now());
+	}
+}
